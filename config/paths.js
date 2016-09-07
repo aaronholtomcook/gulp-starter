8e7855ef1b7153e752bdab6d7f17379e0cabe5f2
Mo Gusbi <mo.gusbi@LC-LTM019.local>
2016-09-07 09:21:57 +0100

    correct path

--- config/paths.js
+++ config/paths.js
@@ -6,15 +6,15 @@ module.exports = {
   node_modules: path.resolve('./node_modules'),
   package: path.resolve('./package.json'),
   src: {
-    fonts: path.json('./src/fonts/**/*.+(eot|woff2|woff|ttf|svg)'),
-    icons: path.json('./src/icons/**/*.svg'),
+    fonts: path.resolve('./src/fonts/**/*.+(eot|woff2|woff|ttf|svg)'),
+    icons: path.resolve('./src/icons/**/*.svg'),
     images: path.resolve('./src/img/**/*.+(gif|jpg|jpeg|png)'),
     js: path.resolve('./src/js/**/*.js'),
     sass: path.resolve('./src/scss/**/*.+(scss|sass)'),
     ts: path.resolve('./src/ts/**/*.ts')
   },
   dest: {
-    fonts: path.json('./dist/assets/fonts'),
+    fonts: path.resolve('./dist/assets/fonts'),
     images: path.resolve('./dist/assets/img'),
     js: path.resolve('./dist/assets/js')
   },

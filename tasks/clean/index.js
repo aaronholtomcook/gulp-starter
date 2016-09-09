b0f42bbec15149fa961602e724b3ddfc0321d47b
Mo Gusbi <mo.gusbi@LC-LTM019.local>
2016-09-09 15:33:55 +0100

    Clean task

---
+++ tasks/clean/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+var paths = require('../../config/paths');
+
+function task (cb) {
+  del(paths.dest.base).then(function () {
+    cb();
+  });
+}
+
+gulp.task('clean', task);
+
+module.exports = task;

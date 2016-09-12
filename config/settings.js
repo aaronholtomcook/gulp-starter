eed5263dfe6c7d8aff1474820ac38c0414fdd696
Mo Gusbi <me@mogusbi.co.uk>
2016-09-12 21:14:24 +0100

    htmlmin setting

--- config/settings.js
+++ config/settings.js
@@ -7,6 +7,7 @@ var config = require(path.resolve('./gulp.config.js')).settings;
 var defaults = {
   angular1: false,
   angular2: false,
+  htmlmin: false,
   scripting: 'js', // Set as "js" for JavaScript, "es6" for ES6 and "ts" for TypeScript
   templates: true,
   uncss: false

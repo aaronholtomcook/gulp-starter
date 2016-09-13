3b576cd5e1006410e7a645d2382bff8922135e6d
Mo Gusbi <mo.gusbi@LC-LTM019.local>
2016-09-13 08:38:11 +0100

    optional html minification

--- tasks/build/index.js
+++ tasks/build/index.js
@@ -7,8 +7,8 @@ var settings = require('../../config/settings');
 function task (cb) {
   sequence(
     'clean',
-    'templates',
     ['copy', 'favicons', 'fonts', 'icons', 'images'],
+    'templates',
     ['scss:lint', settings.scripting === 'ts' ? 'ts:lint' : 'js:lint'],
     ['scss', '' + settings.scripting === 'ts' ? 'ts' : 'js' + ''],
     cb

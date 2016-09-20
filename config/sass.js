cd5eff2ed704bb6f6a468f9e6c768ab9e46228ed
Mo Gusbi <mo.gusbi@LC-LTM019.local>
2016-09-20 11:02:29 +0100

    Fixed path

--- config/sass.js
+++ config/sass.js
@@ -5,7 +5,7 @@ var paths = require('./paths');
 module.exports = {
   includePaths: [
     paths.src.sass,
-    paths.packages.node_modules,
-    paths.packages.bower_components
+    paths.src.packages.node_modules,
+    paths.src.packages.bower_components
   ]
 };

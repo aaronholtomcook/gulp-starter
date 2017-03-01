2c277a8dbae611744c4b5ecce8965c1da06cd19b
Mo Gusbi <mo@Mos-MacBook-Pro.home>
2017-03-01 18:53:57 +0000

    Update task

--- tasks/rev/update.js
+++ tasks/rev/update.js
@@ -3,7 +3,6 @@
 var gulp = require('gulp');
 var rev = require('gulp-rev');
 var revNapkin = require('gulp-rev-napkin');
-var merge = require('merge-stream');
 var path = require('path');
 var paths = require('../../config/paths');
 var config = {
@@ -12,23 +11,18 @@ var config = {
 };
 
 function task () {
-  var css = gulp
-    .src(path.join(paths.dest.css, '/**/*'))
+  return gulp
+    .src([
+      path.join(paths.dest.css, '/**/*.css'),
+      path.join(paths.dest.favicons, '/**/*.{json,webapp,xml}')
+    ], {
+      base: paths.dest.base
+    })
     .pipe(rev())
-    .pipe(gulp.dest(paths.dest.css))
+    .pipe(gulp.dest(paths.dest.base))
     .pipe(revNapkin(config.revNapkin))
     .pipe(rev.manifest(paths.src.templates.manifest, config.rev))
     .pipe(gulp.dest(''));
-
-  var favicon = gulp
-    .src(path.join(paths.dest.favicons, '/**/*.{json,webapp,xml}'))
-    .pipe(rev())
-    .pipe(gulp.dest(paths.dest.favicons))
-    .pipe(revNapkin(config.revNapkin))
-    .pipe(rev.manifest(paths.src.templates.manifest, config.rev))
-    .pipe(gulp.dest(''));
-
-  return merge(css, favicon);
 }
 
 gulp.task('rev:update', task);

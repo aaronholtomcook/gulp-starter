bcd3998bf3daf05fc41c1915b9a8c645e60e96f4
Mo Gusbi <me@mogusbi.co.uk>
2017-03-16 21:14:17 +0000

    Update rev tasks

--- tasks/images/revision.js
+++ tasks/images/revision.js
@@ -7,6 +7,7 @@ const {join} = require('path');
 const paths = require('../../config/paths');
 const errorHandler = require('../../utilities/errorHandler');
 const config = {
+  rev: require('../../config/rev'),
   revNapkin: require('../../config/revNapkin')
 };
 
@@ -17,6 +18,6 @@ gulp.task('images:revision', () => gulp
   .pipe(rev())
   .pipe(gulp.dest(paths.dest.base))
   .pipe(revNapkin(config.revNapkin))
-  .pipe(rev.manifest(paths.manifests.revision))
+  .pipe(rev.manifest(paths.manifests.revision, config.rev))
   .on('error', errorHandler)
   .pipe(gulp.dest('')));

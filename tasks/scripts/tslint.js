d177184ccee3cbc7d4ce8d8fe07e81c6b06dcb52
Mo Gusbi <mo.gusbi@LC-LTM023.local>
2017-03-16 14:35:03 +0000

    Fix linting

--- tasks/scripts/tslint.js
+++ tasks/scripts/tslint.js
@@ -4,13 +4,12 @@ const gulp = require('gulp');
 const tslint = require('gulp-tslint');
 const reporter = require('gulp-tslint-jenkins-reporter');
 const paths = require('../../config/paths');
-const settings = require('../../config/settings');
 const config = {
   tslint: require('../../config/tslint')
 };
 
 gulp.task('scripts:tslint', () => gulp
   .src(paths.src.scripts.files)
-  .pipe(tslint(settings.lint.ts))
+  .pipe(tslint(paths.lint.ts))
   .pipe(tslint.report(config.tslint.report))
   .pipe(reporter(config.tslint.reporter)));

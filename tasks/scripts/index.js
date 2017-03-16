bb9027f2cc29d3cb24e0f18427eec8da2f36e318
Mo Gusbi <me@mogusbi.co.uk>
2017-03-16 20:05:19 +0000

    Webpack stream

--- tasks/scripts/index.js
+++ tasks/scripts/index.js
@@ -2,14 +2,61 @@
 
 const gulp = require('gulp');
 const {log} = require('gulp-util');
-const {spawn} = require('child_process');
+const livereload = require('gulp-livereload');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
+const {notify} = require('node-notifier');
+const paths = require('../../config/paths');
+const config = {
+  webpack: require(paths.config.webpack)
+};
 
 gulp.task('scripts', (cb) => {
-  const webpack = spawn('webpack');
+  let flag = true;
 
-  webpack.stdout.on('data', (data) => log(`stdout: ${data}`));
+  return webpackStream(config.webpack, webpack, (err, stats) => {
+    if (err || stats.compilation.errors.length > 0) {
+      log('[Webpack: Error]', stats.compilation.errors);
 
-  webpack.stderr.on('data', (data) => log(`stderr: ${data}`));
+      notify({
+        title: '[Webpack: Error]',
+        message: stats.compilation.errors[0].message,
+        sound: true,
+        type: 'error'
+      });
 
-  webpack.on('close', () => cb());
+      if (flag) {
+        flag = false;
+
+        return cb();
+      }
+    } else {
+      log('[Webpack: Build]', stats.toString({
+        colors: gutil.colors.supportsColor,
+        hash: false,
+        version: false,
+        timings: true,
+        assets: false,
+        chunks: false,
+        chunkModules: false,
+        modules: false,
+        children: false,
+        cached: false,
+        reasons: false,
+        source: false,
+        errorDetails: false,
+        chunkOrigins: false
+      }));
+
+      if (flag) {
+        flag = false;
+
+        if (config.webpack.watch) {
+          return cb();
+        }
+      } else {
+        livereload();
+      }
+    }
+  });
 });

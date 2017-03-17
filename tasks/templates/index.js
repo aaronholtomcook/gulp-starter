8c54e0c182291d672f762091fc081a115d2efc1e
Mo Gusbi <me@mogusbi.co.uk>
2017-03-17 23:19:20 +0000

    Fix html reload issue

--- tasks/templates/index.js
+++ tasks/templates/index.js
@@ -18,9 +18,8 @@ const config = {
 gulp.task('templates', () => gulp
   .src(paths.src.templates.pages)
   .pipe(data(JSON.parse(readFileSync(paths.src.templates.data, 'utf8'))))
-  .on('error', errorHandler)
   .pipe(nunjucks(config.nunjucks))
-  .on('error', errorHandler)
   .pipe(gulpIf(process.env.NODE_ENV === 'production' && settings.htmlmin, htmlmin(config.htmlmin))) // Minify html for production if enabled
+  .on('error', errorHandler)
   .pipe(gulp.dest(paths.dest.base))
-  .on('end', () => livereload()));
+  .pipe(livereload()));

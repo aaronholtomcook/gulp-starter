e12c0d8705a3817f4ae5eb48db2ee91188d4b014
Mo Gusbi <me@mogusbi.co.uk>
2017-03-18 22:56:24 +0000

    Sort out live reload

--- utilities/server.js
+++ utilities/server.js
@@ -2,16 +2,20 @@
 
 'use strict';
 
+const express = require('express');
 const livereload = require('connect-livereload');
 const settings = require('../config/settings');
 const paths = require('../config/paths');
 const app = require(paths.server.app);
 
+const server = express();
+
 if (process.env.NODE_ENV === 'development') {
-  app.use(livereload());
+  server.use(livereload());
 }
 
-app
+server
+  .use(app)
   .set('port', settings.port)
   .listen(settings.port, () => console.log(`
 ==========================================

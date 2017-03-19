32865a3fef9a801dd6edf0aeee40d8bdfc995d79
Mo Gusbi <me@mogusbi.co.uk>
2017-03-19 16:39:13 +0000

    Linting favicons

--- utilities/favicons/index.js
+++ utilities/favicons/index.js
@@ -3,7 +3,6 @@
 const jimp = require('jimp');
 const jsonxml = require('jsontoxml');
 const mkdirp = require('mkdirp');
-const {error} = require('util');
 const {writeFile} = require('fs');
 const {dirname, extname, join} = require('path');
 const paths = require('../../config/paths');
@@ -15,98 +14,121 @@ module.exports = (config) => {
     android: {
       'android-chrome-144x144.png': {
         height: 144,
+        transparent: true,
         width: 144
       },
       'android-chrome-192x192.png': {
         height: 192,
+        transparent: true,
         width: 192
       },
       'android-chrome-256x256.png': {
         height: 256,
+        transparent: true,
         width: 256
       },
       'android-chrome-36x36.png': {
         height: 36,
+        transparent: true,
         width: 36
       },
       'android-chrome-384x384.png': {
         height: 384,
+        transparent: true,
         width: 384
       },
       'android-chrome-48x48.png': {
         height: 48,
+        transparent: true,
         width: 48
       },
       'android-chrome-512x512.png': {
         height: 512,
+        transparent: true,
         width: 512
       },
       'android-chrome-72x72.png': {
         height: 72,
+        transparent: true,
         width: 72
       },
       'android-chrome-96x96.png': {
         height: 96,
+        transparent: true,
         width: 96
       }
     },
     apple: {
       'apple-touch-icon-114x114.png': {
         height: 114,
+        transparent: false,
         width: 114
       },
       'apple-touch-icon-120x120.png': {
         height: 120,
+        transparent: false,
         width: 120
       },
       'apple-touch-icon-144x144.png': {
         height: 144,
+        transparent: false,
         width: 144
       },
       'apple-touch-icon-152x152.png': {
         height: 152,
+        transparent: false,
         width: 152
       },
       'apple-touch-icon-167x167.png': {
         height: 167,
+        transparent: false,
         width: 167
       },
       'apple-touch-icon-180x180.png': {
         height: 180,
+        transparent: false,
         width: 180
       },
       'apple-touch-icon-57x57.png': {
         height: 57,
+        transparent: false,
         width: 57
       },
       'apple-touch-icon-60x60.png': {
         height: 60,
+        transparent: false,
         width: 60
       },
       'apple-touch-icon-72x72.png': {
         height: 72,
+        transparent: false,
         width: 72
       },
       'apple-touch-icon-76x76.png': {
         height: 76,
+        transparent: false,
         width: 76
       },
       'apple-touch-icon-precomposed.png': {
         height: 180,
+        transparent: false,
         width: 180
       },
       'apple-touch-icon.png': {
         height: 180,
+        transparent: false,
         width: 180
       }
     },
     favicons: {
       'favicon-16x16.png': {
         height: 180,
+        transparent: true,
         width: 180
       },
       'favicon-32x32.png': {
         height: 180,
+        transparent: true,
         width: 180
       },
       'favicon.ico': {
@@ -125,28 +147,34 @@ module.exports = (config) => {
         }, {
           height: 64,
           width: 64
-        }]
+        }],
+        transparent: true
       }
     },
     windows: {
       'mstile-144x144.png': {
         height: 144,
+        transparent: true,
         width: 144
       },
       'mstile-150x150.png': {
         height: 270,
+        transparent: true,
         width: 270
       },
       'mstile-310x150.png': {
         height: 270,
+        transparent: true,
         width: 558
       },
       'mstile-310x310.png': {
         height: 558,
+        transparent: true,
         width: 558
       },
       'mstile-70x70.png': {
         height: 128,
+        transparent: true,
         width: 128
       }
     }
@@ -155,7 +183,7 @@ module.exports = (config) => {
   const manifests = {
     android: {
       'manifest.json': {
-        background_color: config.app.background,
+        'background_color': config.app.background,
         display: 'standalone',
         icons: [{
           sizes: '36x36',
@@ -315,29 +343,29 @@ module.exports = (config) => {
   function createManifests (platform) {
     for (const manifest in manifests[platform]) {
       if (manifests[platform].hasOwnProperty(manifest)) {
-        let contents;
+        let contents = '';
 
         switch (extname(manifest)) {
-          case '.json':
-            contents = JSON.stringify(manifests[platform][manifest], null, '\t');
-            break;
-          case '.xml':
-            contents = jsonxml(manifests[platform][manifest], {
-              prettyPrint: true,
-              xmlHeader: true
-            });
-            break;
-          default:
+        case '.json':
+          contents = JSON.stringify(manifests[platform][manifest], null, '\t');
+          break;
+        case '.xml':
+          contents = jsonxml(manifests[platform][manifest], {
+            prettyPrint: true,
+            xmlHeader: true
+          });
+          break;
+        default:
         }
 
         promises.push(
           new Promise(
             (resolve, reject) => mkdirp(
               config.output.icons,
-              (err) => err ? reject(err) : writeFile(
+              (mkdirpErr) => mkdirpErr ? reject(mkdirpErr) : writeFile(
                 join(config.output.icons, manifest),
                 contents,
-                (err) => err ? reject(err) : resolve(contents)
+                (writeFileErr) => writeFileErr ? reject(writeFileErr) : resolve(contents)
               )
             )
           )
@@ -350,14 +378,16 @@ module.exports = (config) => {
   function createMetaData () {
     let output = '';
 
+    function writeTag (tag) {
+      if ((tag.app && config.appEnabled) || !tag.app) {
+        output += `${tag.template}
+`;
+      }
+    }
+
     for (const platform in metaData) {
       if (metaData.hasOwnProperty(platform)) {
-        metaData[platform].forEach((tag) => {
-          if ((tag.app && config.appEnabled) || !tag.app) {
-            output += `${tag.template}
-`;
-          }
-        });
+        metaData[platform].forEach(writeTag);
       }
     }
 
@@ -365,11 +395,11 @@ module.exports = (config) => {
       new Promise(
         (resolve, reject) => mkdirp(
           dirname(paths.src.favicons.output),
-          (err) => err ? reject(err) : writeFile(
-              paths.src.favicons.output,
-              output,
-              (err) => err ? reject(err) : resolve(output)
-            )
+          (mkdirpErr) => mkdirpErr ? reject(mkdirpErr) : writeFile(
+            paths.src.favicons.output,
+            output,
+            (writeFileErr) => writeFileErr ? reject(writeFileErr) : resolve(output)
+          )
         )
       )
     );
@@ -388,9 +418,7 @@ module.exports = (config) => {
   return Promise.all(promises)
     .catch(
       (err) => {
-        error(err);
-
-        process.exit(1);
+        throw new Error(err);
       }
     );
 };

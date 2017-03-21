f560efe872410bfb427d53e2ad84469579825090
Mo Gusbi <me@mogusbi.co.uk>
2017-03-21 20:18:17 +0000

    Try to kill tslint if you're not using typescript so it's not a forced dependency

--- tasks/scripts/eslint.js
+++ tasks/scripts/eslint.js
@@ -7,10 +7,13 @@ const mkdirp = require('mkdirp');
 const {writeFileSync} = require('fs');
 const {dirname} = require('path');
 const paths = require('../../config/paths');
+const settings = require('../../config/settings');
 
-gulp.task('scripts:eslint', () => gulp
-  .src(paths.src.scripts)
-  .pipe(eslint(paths.lint.js))
-  .pipe(eslint.format())
-  .pipe(eslint.format('checkstyle', (results) => mkdirp(dirname(paths.reports.js), () => writeFileSync(paths.reports.js, results))))
-  .pipe(gulpIf(process.env.NODE_ENV === 'production', eslint.failAfterError())));
+if (settings.scripting === 'js') {
+  gulp.task('scripts:eslint', () => gulp
+    .src(paths.src.scripts)
+    .pipe(eslint(paths.lint.js))
+    .pipe(eslint.format())
+    .pipe(eslint.format('checkstyle', (results) => mkdirp(dirname(paths.reports.js), () => writeFileSync(paths.reports.js, results))))
+    .pipe(gulpIf(process.env.NODE_ENV === 'production', eslint.failAfterError())));
+}
--- tasks/scripts/tslint.js
+++ tasks/scripts/tslint.js
@@ -4,12 +4,15 @@ const gulp = require('gulp');
 const tslint = require('gulp-tslint');
 const reporter = require('gulp-tslint-jenkins-reporter');
 const paths = require('../../config/paths');
+const settings = require('../../config/settings');
 const config = {
   tslint: require('../../config/tslint')
 };
 
-gulp.task('scripts:tslint', () => gulp
-  .src(paths.src.scripts)
-  .pipe(tslint(config.tslint.config))
-  .pipe(tslint.report(config.tslint.report))
-  .pipe(reporter(config.tslint.reporter)));
+if (settings.scripting === 'ts') {
+  gulp.task('scripts:tslint', () => gulp
+    .src(paths.src.scripts)
+    .pipe(tslint(config.tslint.config))
+    .pipe(tslint.report(config.tslint.report))
+    .pipe(reporter(config.tslint.reporter)));
+}

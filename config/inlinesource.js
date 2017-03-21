c6df282912799dbb6d362ed110d9fe665086ad41
Mo Gusbi <me@mogusbi.co.uk>
2017-03-21 21:19:38 +0000

    Update inlinesource config

--- config/inlinesource.js
+++ config/inlinesource.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const paths = require('./paths');
 const config = require('../utilities/config');
 
 const defaults = {
-  compress: false
+  compress: false,
+  rootpath: paths.dest.base
 };
 
 module.exports = config(defaults, 'inlinesource');

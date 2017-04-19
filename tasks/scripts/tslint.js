8d134ad732931e6d15a669490e7447f177d27f23
Mo Gusbi <mo.gusbi@LC-LTM023.local>
2017-04-19 14:47:41 +0100

    Attempt type checking

--- tasks/scripts/tslint.js
+++ tasks/scripts/tslint.js
@@ -4,16 +4,25 @@ const settings = require('../../config/settings');
 
 if (settings.scripting === 'ts') {
   const gulp = require('gulp');
-  const tslint = require('gulp-tslint');
+  const linter = require('gulp-tslint');
   const reporter = require('gulp-tslint-jenkins-reporter');
+  const tslint = require('tslint');
   const paths = require('../../config/paths');
   const config = {
     tslint: require('../../config/tslint')
   };
 
-  gulp.task('scripts:tslint', () => gulp
-    .src(paths.src.scripts)
-    .pipe(tslint(config.tslint.config))
-    .pipe(tslint.report(config.tslint.report))
-    .pipe(reporter(config.tslint.reporter)));
+  gulp.task('scripts:tslint', () => {
+    const program = tslint.Linter.createProgram(paths.lint.ts);
+
+    return gulp
+      .src(paths.src.scripts, {
+        base: '.'
+      })
+      .pipe(linter({
+        program
+      }))
+      .pipe(linter.report(config.tslint.report))
+      .pipe(reporter(config.tslint.reporter));
+  });
 }
